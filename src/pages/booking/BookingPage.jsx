8ef1081a600ed87f75f91406694252977e55703f
Julien Fleury <you@example.comjulienfleury93@gmail.com>
2022-02-09 15:07:56 +0100

    Setting up the basic folder structure and the first few pages.

---
+++ src/pages/booking/BookingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './BookingPage.styles.scss';
+
+class BookingPage extends React.Component {
+    render() {
+        return(
+            <div className="bookingContainer">
+
+            </div>
+        );
+    }
+}
+
+export default BookingPage;
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.styles.scss';
+
+class HomePage extends React.Component {
+    render() {
+        return(
+            <div className='homeContainer'>
+
+            </div>
+        );
+    }
+}
+
+export default HomePage;
