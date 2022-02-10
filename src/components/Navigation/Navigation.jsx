b52c5263da78dfa3615a1f2b0597a41fb8f31f8f
Julien Fleury <you@example.comjulienfleury93@gmail.com>
2022-02-10 08:55:25 +0100

    Adding the start of routing and working on navigation.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import './Navigation.styles.scss';
+import { Link } from "react-router-dom";
+import logo from './logo.svg';
 
 const Navigation = () => {
-    return(
-        <nav>
-
+    return (
+        <nav className="mainNavigation">
+            <Link className="nav-item" to={'/#'} >Services</Link>
+            <Link className="nav-item" to={'/#'} >Pricing</Link>
+            <Link className="nav-item" to={'/#'} >About</Link>
+            <Link className="nav-item" to={'/#'} >
+                <img src={logo} className="logo" alt="logo" />
+            </Link>
+            <Link className="nav-link" to={'/#'} >Services</Link>
+            <Link className="nav-link" to={'/#'} >Services</Link>
+            <Link className="nav-link" to={'/#'} >Services</Link>
         </nav>
     );
 }
