6bb8187dc616fcfaac4b07a88dc208667313d69b
Julien Fleury <you@example.comjulienfleury93@gmail.com>
2022-02-20 21:25:39 +0100

    Updated the service list and added the packages.

--- src/ServiceList.js
+++ src/ServiceList.js
@@ -7,8 +7,36 @@ export const ServiceList = [
         id: 1,
         pricing: '$150',
         services: [
-            'Professional real estate photography',
-            'Branded and unbranded property websites'
+            'Professional real estate photography.',
+            'Branded and unbranded property websites.',
+            '24 hour turnaround.'
+        ],
+        productVariants: [
+            {
+                size: '0-3000',
+                numberOfImages: 25,
+                price: '$150'
+            },
+            {
+                size: '3001-4000',
+                numberOfImages: 30,
+                price: '$175'
+            },
+            {
+                size: '4001-5000',
+                numberOfImages: 35,
+                price: '$200'
+            },
+            {
+                size: '5001-6000',
+                numberOfImages: 40,
+                price: '$225'
+            },
+            {
+                size: '6000+',
+                numberOfImages: 50,
+                price: '$250'
+            },
         ],
         timeBracket: 'short', //Maybe another way?
         mainImageUrl: `url(${testImageOne})`,
@@ -19,8 +47,34 @@ export const ServiceList = [
         id: 2,
         pricing: '$150',
         services: [
-            'Replace this',
-            'Replace this too'
+            'Update This',
+        ],
+        productVariants: [
+            {
+                size: '0-3000',
+                numberOfImages: 25,
+                price: '$150'
+            },
+            {
+                size: '3001-4000',
+                numberOfImages: 30,
+                price: '$175'
+            },
+            {
+                size: '4001-5000',
+                numberOfImages: 35,
+                price: '$200'
+            },
+            {
+                size: '5001-6000',
+                numberOfImages: 40,
+                price: '$225'
+            },
+            {
+                size: '6000+',
+                numberOfImages: 50,
+                price: '$250'
+            },
         ],
         timeBracket: 'short',
         mainImageUrl: `url(${testImageOne})`,
@@ -31,8 +85,35 @@ export const ServiceList = [
         id: 3,
         pricing: '$150',
         services: [
-            'Up to 20 professional aerial drone photos',
-            'You can order drone photography at a discounted rate when selecting another base service',
+            'Up to 20 professional aerial drone photos.',
+            'This is a base service. You can order drone photography at a discounted rate when selecting another base service.'
+        ],
+        productVariants: [
+            {
+                size: '0-3000',
+                numberOfImages: 25,
+                price: '$150'
+            },
+            {
+                size: '3001-4000',
+                numberOfImages: 30,
+                price: '$175'
+            },
+            {
+                size: '4001-5000',
+                numberOfImages: 35,
+                price: '$200'
+            },
+            {
+                size: '5001-6000',
+                numberOfImages: 40,
+                price: '$225'
+            },
+            {
+                size: '6000+',
+                numberOfImages: 50,
+                price: '$250'
+            },
         ],
         timeBracket: 'short',
         mainImageUrl: `url(${testImageTwo})`,
@@ -43,8 +124,36 @@ export const ServiceList = [
         id: 4,
         pricing: '$125',
         services: [
-            '360 Matterport tour',
-            'Branded and unbranded property websites'
+            '360 Matterport tour.',
+            'Branded and unbranded property websites.',
+            '24 hour turnaround'
+        ],
+        productVariants: [
+            {
+                size: '0-3000',
+                numberOfImages: null,
+                price: '$125'
+            },
+            {
+                size: '3001-4000',
+                numberOfImages: null,
+                price: '$150'
+            },
+            {
+                size: '4001-5000',
+                numberOfImages: null,
+                price: '$200'
+            },
+            {
+                size: '5001-6000',
+                numberOfImages: null,
+                price: '$250'
+            },
+            {
+                size: '6000+',
+                numberOfImages: null,
+                price: '$300'
+            },
         ],
         timeBracket: 'long',
         mainImageUrl: `url(${testImageOne})`,
@@ -55,8 +164,34 @@ export const ServiceList = [
         id: 5,
         pricing: '$150',
         services: [
-            'Replace this',
-            'Replace this too'
+            'Update This',
+        ],
+        productVariants: [
+            {
+                size: '0-3000',
+                numberOfImages: 25,
+                price: '$150'
+            },
+            {
+                size: '3001-4000',
+                numberOfImages: 30,
+                price: '$175'
+            },
+            {
+                size: '4001-5000',
+                numberOfImages: 35,
+                price: '$200'
+            },
+            {
+                size: '5001-6000',
+                numberOfImages: 40,
+                price: '$225'
+            },
+            {
+                size: '6000+',
+                numberOfImages: 50,
+                price: '$250'
+            },
         ],
         timeBracket: 'short',
         mainImageUrl: `url(${testImageOne})`,
@@ -67,9 +202,38 @@ export const ServiceList = [
         id: 6,
         pricing: '$200',
         services: [
-            'Highlight your listing and get more views',
-            "Don't just sell the home, sell the lifestyle",
-            'Drone video included'
+            'Highlight your listing and get more views.',
+            "Don't just sell the home, sell the lifestyle.",
+            'Drone video included.',
+            'Branded and unbranded property websites.',
+            '48 hour turnaround.'
+        ],
+        productVariants: [
+            {
+                size: '0-3000',
+                numberOfImages: null,
+                price: '$200'
+            },
+            {
+                size: '3001-4000',
+                numberOfImages: null,
+                price: '$250'
+            },
+            {
+                size: '4001-5000',
+                numberOfImages: null,
+                price: '$300'
+            },
+            {
+                size: '5001-6000',
+                numberOfImages: null,
+                price: '$350'
+            },
+            {
+                size: '6000+',
+                numberOfImages: null,
+                price: '$400'
+            },
         ],
         timeBracket: 'long',
         mainImageUrl: `url(${testImageOne})`,
@@ -80,8 +244,34 @@ export const ServiceList = [
         id: 7,
         pricing: '$150',
         services: [
-            'Replace this',
-            'Replace this too'
+            'Update This',
+        ],
+        productVariants: [
+            {
+                size: '0-3000',
+                numberOfImages: 25,
+                price: '$150'
+            },
+            {
+                size: '3001-4000',
+                numberOfImages: 30,
+                price: '$175'
+            },
+            {
+                size: '4001-5000',
+                numberOfImages: 35,
+                price: '$200'
+            },
+            {
+                size: '5001-6000',
+                numberOfImages: 40,
+                price: '$225'
+            },
+            {
+                size: '6000+',
+                numberOfImages: 50,
+                price: '$250'
+            },
         ],
         timeBracket: 'short',
         mainImageUrl: `url(${testImageOne})`,
@@ -92,8 +282,34 @@ export const ServiceList = [
         id: 8,
         pricing: '$150',
         services: [
-            'Replace this',
-            'Replace this too'
+            'Update This',
+        ],
+        productVariants: [
+            {
+                size: '0-3000',
+                numberOfImages: 25,
+                price: '$150'
+            },
+            {
+                size: '3001-4000',
+                numberOfImages: 30,
+                price: '$175'
+            },
+            {
+                size: '4001-5000',
+                numberOfImages: 35,
+                price: '$200'
+            },
+            {
+                size: '5001-6000',
+                numberOfImages: 40,
+                price: '$225'
+            },
+            {
+                size: '6000+',
+                numberOfImages: 50,
+                price: '$250'
+            },
         ],
         timeBracket: 'long',
         mainImageUrl: `url(${testImageOne})`,
@@ -104,11 +320,136 @@ export const ServiceList = [
         id: 9,
         pricing: '$150',
         services: [
-            'Replace this',
-            'Replace this too'
+            'Update This',
+        ],
+        productVariants: [
+            {
+                size: '0-3000',
+                numberOfImages: 25,
+                price: '$150'
+            },
+            {
+                size: '3001-4000',
+                numberOfImages: 30,
+                price: '$175'
+            },
+            {
+                size: '4001-5000',
+                numberOfImages: 35,
+                price: '$200'
+            },
+            {
+                size: '5001-6000',
+                numberOfImages: 40,
+                price: '$225'
+            },
+            {
+                size: '6000+',
+                numberOfImages: 50,
+                price: '$250'
+            },
         ],
         timeBracket: 'short',
         mainImageUrl: `url(${testImageOne})`,
         linkAddress: 'product-photography'
     }
+];
+
+export const Packages = [
+    {
+        id: 1,
+        service: 'Drone Photography & Drone Video Package',
+        services: [
+            'This package is perfect for plots of land and ranches.',
+            '15 aerial drone photos.',
+            'Drone video of the property.',
+            'Branded and unbranded property website.',
+            '48 hour turnaround.'
+        ],
+        price: '$225'
+    },
+    {
+        id: 2,
+        service: 'Deluxe Package',
+        services: [
+            'Professional real estate photography.',
+            'Property video of your listing.',
+            '360 Matterport Tour.',
+            'Branded and unbranded property website.',
+            '48 hour turnaround.'
+        ],
+        price: '$400',
+        productVariants: [
+            {
+                size: '0-3000',
+                numberOfImages: 25,
+                price: '$400'
+            },
+            {
+                size: '3001-4000',
+                numberOfImages: 30,
+                price: '$450'
+            },
+            {
+                size: '4001-5000',
+                numberOfImages: 35,
+                price: '$500'
+            },
+            {
+                size: '5001-6000',
+                numberOfImages: 40,
+                price: '$550'
+            },
+            {
+                size: '6000+',
+                numberOfImages: 50,
+                price: '$600'
+            },
+        ],
+    },
+    {
+        id: 3,
+        service: 'Premier Package',
+        services: [
+            'Professional real estate photography.',
+            'Property video of your listing.',
+            'Drone video of the property.',
+            '360 Matterport tour.',
+            '5 Aerial drone photos of your listing.',
+            '5 Neighborhood amenity photos.',
+            'Property lines inserted on select aerial photos.',
+            '2D floor plan included.',
+            'Branded and unbranded property website.',
+            '48 hour turnaround.'
+        ],
+        price: '$500',
+        productVariants: [
+            {
+                size: '0-3000',
+                numberOfImages: 25,
+                price: '$500'
+            },
+            {
+                size: '3001-4000',
+                numberOfImages: 30,
+                price: '$550'
+            },
+            {
+                size: '4001-5000',
+                numberOfImages: 35,
+                price: '$600'
+            },
+            {
+                size: '5001-6000',
+                numberOfImages: 40,
+                price: '$650'
+            },
+            {
+                size: '6000+',
+                numberOfImages: 50,
+                price: '$700'
+            },
+        ],
+    },
+    
 ];
