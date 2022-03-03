9b6f2e37f9ba55932cccb188187699cdda82678b
Julien Fleury <you@example.comjulienfleury93@gmail.com>
2022-03-03 17:32:33 +0100

    Added firebase drawing data to populate the information on website.

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -12,12 +12,25 @@ import Footer from '../../components/Footer/Footer.component';
 import { firestore } from '../../firebase/firebase.utils';
 
 class HomePage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            heroBody: 'Video, Photography, Drone. We pride ourselves on producing the highest quality content for your business needs. Telling your story allows for your customers to connect with you and understand why you do what you do. Nowadays people consume content at an astronomical rate. You need to be able to standout in an ever growing crowd. Let us help tell your story.'
+        }
+    }
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
         const textChangesRef = firestore.collection('TextChanges');
         textChangesRef.onSnapshot(async snapshot => {
-            console.log(snapshot);
+            const docArray = snapshot.docs;
+
+            // Homepage
+            const homePageData = docArray[0].data();
+            this.setState({
+                heroBody: homePageData.HeroBody
+            });
+
         });
     }
     render() {
@@ -55,7 +68,7 @@ class HomePage extends React.Component {
                 <div className="row m-0">
                     <div className="col-12 info-text">
                         <h2>HIGH QUALITY CONTENT{<br />}TO TELL YOUR STORY</h2>
-                        <p>Video, Photography, Drone. We pride ourselves on producing the highest quality content for your business needs. Telling your story allows for your customers to connect with you and understand why you do what you do. Nowadays people consume content at an astronomical rate. You need to be able to standout in an ever growing crowd. Let us help tell your story.</p>
+                        <p>{this.state.heroBody}</p>
                     </div>
                 </div>
                 <div className="row m-0 d-flex justify-content-center">
